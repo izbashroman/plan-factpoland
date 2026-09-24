73f695fb6ba8095dbcb40faaee8955c47435f062
izbashroman <izbash.roman@gmail.com>
2026-09-24 17:09:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Пользователи: каждая строка — зашифрованная логином и паролем ссылка на таблицу.
 // Новую строку делает setup.html. Удалите строку — пользователь потеряет доступ.
 window.PF_USERS=[
-  {"s":"j4DNBT1tJq1i+mPp++7b7g==","v":"BTbLBWpKbRu/el8q","i":150000,"c":"5kx3WG7lWIOuPrJlIwBJB/8B9zWwpK9XobnVQ+rIICIDnbsDSaDSUQToWp9SSyvX1Vnz14tpdUhohSBgaB58FuB71tpjl70v7QcWf93XT9OCWJPsZxVj1XwpqPq/YInftV2WErnyC7TpDEDO83/2lQHByzDx9eQYmJKv4/lGey+lqFqCghcdDnRJJDhWzRp8+eksada3+P3fjHieA75fpkZqzygOYh9cKdJQcD1WL1UTQihQTyYgkUMHdjHqU32fg02h/wAXVzLdnXmh"} // admin
-  {"s":"IfVkZB0jBkbVyh0ab9f6pA==","v":"nhjAMUvDwYdZj8/8","i":150000,"c":"D6RZgO9/uO7gO+Oh22sR8cttdxIMP6DzqIxc0ii93zy/UqbbIpm2j/9j1B1DFmZou2NKtiesHuhIyGb+WhXAlZFebHDg9s27w1ncOMcvmKp+nZA5QrfDyfCqdrEdqQXlTWmnmfIdmfsYyTWptjtCUEkmLvyTsGpJdonCqDRtSxyVo+wAgkgg8j3Ydwn5MJ9ptxmorRg85Qo85HZodgf7l2HEltDi6NAjbqe1EGlwnclK+hYODAMq9WzPnjf0Amy0aUmwEHs/IH4="}, // Inna
+  {"s":"j4DNBT1tJq1i+mPp++7b7g==","v":"BTbLBWpKbRu/el8q","i":150000,"c":"5kx3WG7lWIOuPrJlIwBJB/8B9zWwpK9XobnVQ+rIICIDnbsDSaDSUQToWp9SSyvX1Vnz14tpdUhohSBgaB58FuB71tpjl70v7QcWf93XT9OCWJPsZxVj1XwpqPq/YInftV2WErnyC7TpDEDO83/2lQHByzDx9eQYmJKv4/lGey+lqFqCghcdDnRJJDhWzRp8+eksada3+P3fjHieA75fpkZqzygOYh9cKdJQcD1WL1UTQihQTyYgkUMHdjHqU32fg02h/wAXVzLdnXmh"}, // admin
+  {"s":"q+E3jdJXtuB9AxSyNR0cag==","v":"U3947nZl95qLfhR7","i":150000,"c":"SmfXOjYlsLJMi32XY/opcp2l7yVgFPoKcJjPmODamrN3mhaTMwASMpL1HXDYN/vFmqz93YW3yrQNfMl8Y3LcqWwmrX7P9J+Lto1bHSyjTj5ndVnhmBC9S7WqL04wrxvSR2mk9Au6+1DeQ2tdOaEmHmubpUppZMK57EefjLIK7RM+Gf4C9AkS2fLwplYQWed/mSrrPxrC946+HU5tgHhXzBr08I2OVTJMSrNrwSjAdDYesfRkLc/JAIGsRYUn2NMcZij7tq9qGgg="}, // Inna
 ];
