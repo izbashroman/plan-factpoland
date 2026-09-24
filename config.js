d5988be1b3f7f7763f57822929b16725bc50cd80
izbashroman <izbash.roman@gmail.com>
2026-09-24 16:48:22 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Пользователи: каждая строка — зашифрованная логином и паролем ссылка на таблицу.
+// Новую строку делает setup.html. Удалите строку — пользователь потеряет доступ.
+window.PF_USERS=[
+  {"s":"j4DNBT1tJq1i+mPp++7b7g==","v":"BTbLBWpKbRu/el8q","i":150000,"c":"5kx3WG7lWIOuPrJlIwBJB/8B9zWwpK9XobnVQ+rIICIDnbsDSaDSUQToWp9SSyvX1Vnz14tpdUhohSBgaB58FuB71tpjl70v7QcWf93XT9OCWJPsZxVj1XwpqPq/YInftV2WErnyC7TpDEDO83/2lQHByzDx9eQYmJKv4/lGey+lqFqCghcdDnRJJDhWzRp8+eksada3+P3fjHieA75fpkZqzygOYh9cKdJQcD1WL1UTQihQTyYgkUMHdjHqU32fg02h/wAXVzLdnXmh"} // admin
+];
